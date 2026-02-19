783c89c36ed0d550eb4239c00d21956abe4857f8
Skelci <136198665+skelci@users.noreply.github.com>
2026-02-19 18:35:45 +0100

    Implement error handling after compile script
    Add error handling for compile script execution

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -4,5 +4,7 @@ $ucrt = "C:\msys64\ucrt64\bin"
 $env:Path = "$ucrt;$env:Path"
 
 ./scripts/compile.ps1
+if ($LastExitCode -ne 0) { exit $LastExitCode }
+
 Write-Host -ForegroundColor Green "====== Running the application... ======"
 ./build/CppTemplate.exe
